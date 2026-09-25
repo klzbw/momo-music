37a0ee53ea08e1fc1995014b23265751cd35250a
klzbw <klzbw@users.noreply.github.com>
2026-09-25 10:19:58 +0800

    ci: separate capacitor.config.ts, fix YAML

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.klzbw.momomusic',
+  appName: 'momo-music',
+  webDir: 'dist',
+  backgroundColor: '#000000',
+};
+
+export default config;
